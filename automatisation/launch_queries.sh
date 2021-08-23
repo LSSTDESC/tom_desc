7a067edf48c2f8b7f405742f43e310416c1ef2a1
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2021-08-23 20:33:23 +0200

    add a boilerplate lanch_queries.sh for cron jobs

--- automatisation/launch_queries.sh
+++ automatisation/launch_queries.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-
-TOMBASEFOLDER=/home/ubuntu/tom-marshall
+TOMBASE=tom_desc
+TOMBASEFOLDER=/home/cohen/lsst/alerts/
 NIGHT=`date +"%Y%m%d"`
 
-cd $TOMBASEFOLDER
+source ${TOMBASEFOLDER}/tom_env/bin/activate
+export DB_HOST=localhost
+export DB_PASS=postgres
+
+cd $TOMBASEFOLDER/${TOMBASE}
 
 # Fink
 QUERYNAME=early_sn_ia_last_day
+#QUERYNAME=fink_sso_ztf_candidates_ztf
 LISTNAME=fink_${QUERYNAME}_${NIGHT}
+python manage.py run_broker_query $QUERYNAME $LISTNAME
+
+QUERYNAME=pseudo_sn
+LISTNAME=antares_${QUERYNAME}_${NIGHT}
 python $TOMBASEFOLDER/manage.py run_broker_query $QUERYNAME $LISTNAME
 
 # Add more queries here
