c7e2e981cf9e767b48449f6f222adfc123428f22
troyraen <troy.raen@pitt.edu>
2021-10-17 12:06:52 -0400

    update comments and get_parser_classes

--- stream/management/commands/pittgoogle.py
+++ stream/management/commands/pittgoogle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 """Listen to a Pitt-Google Pub/Sub stream and save alerts to the database."""
+from importlib import import_module
+import logging
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
@@ -11,17 +14,30 @@ from stream.models import Alert, Topic
 from pittgoogle import Consumer
 
 
+logger = logging.getLogger(__name__)
+
 PITTGOOGLE_CONSUMER_CONFIGURATION = settings.PITTGOOGLE_CONSUMER_CONFIGURATION
 PITTGOOGLE_PARSERS = settings.PITTGOOGLE_PARSERS
 
 
 def get_parser_classes(topic):
+    """."""
     parser_classes = []
 
-    parsers = PITTGOOGLE_PARSERS[topic]
+    try:
+        parsers = PITTGOOGLE_PARSERS[topic]
+    except KeyError:
+        logger.log(msg=f'PITTGOOGLE_PARSERS not found for topic: {topic}.', level=logging.WARNING)
+        return parser_classes
+
     for parser in parsers:
-        # TODO
-        pass
+        mod_name, class_name = parser.rsplit('.', 1)
+        try:
+            mod = import_module(mod_name)
+            clazz = getattr(mod, class_name)
+        except (ImportError, AttributeError):
+            raise ImportError(f'Unable to import parser {parser}')
+        parser_classes.append(clazz)
 
     return parser_classes
 
@@ -31,16 +47,22 @@ class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.consumer = Consumer(PITTGOOGLE_CONSUMER_CONFIGURATION['SUBSCRIPTION_NAME'])
+        self.consumer = Consumer(PITTGOOGLE_CONSUMER_CONFIGURATION['subscription_name'])
 
     def handle(self, *args, **options):
-        """Pull and process alerts through the `callback` (`parse_and_save`),
-        in a background thread, until a stoping condition is reached.
-        Block until the processing is complete and the connection has been closed.
+        """Pull and process alerts from a Pitt-Google Pub/Sub stream.
+
+        -   Execute a "streaming pull" in a background thread.
+            Asyncronously, pull alerts and process them through the callback,
+            `parse_and_save`.
+
+        -   Stop when a stoping condition is reached.
+
+        -   Block until the processing is complete and the connection has been closed.
         """
         self.consumer.stream_alerts(
+            callback=self.parse_and_save,
             stop_conditions=PITTGOOGLE_CONSUMER_CONFIGURATION['stop_conditions'],
-            callback=self.parse_and_save
         )
 
         # If you'd prefer to iterate over the alerts, it would look like this:
@@ -48,8 +70,8 @@ class Command(BaseCommand):
         # for alert in alert_list:
         #     self.parse_and_save(alert, self.consumer.topic_name)
 
-
     def parse_and_save(alert, topic_name):
+        """Parse the alert and save to the database."""
         topic, _ = Topic.objects.get_or_create(name=topic_name)
         alert = Alert.objects.create(topic=topic, raw_message=alert.data)
 
--- stream/parsers/pittgoogle_alert_parser.py
+++ stream/parsers/pittgoogle_alert_parser.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class PittGoogleAlertParser(BaseParser):
+    # TODO: write this
 
     def parse(self):
-        # TODO
         pass
