14e87c8c41350ef83f266946faf42a7efb3e9a8d
troyraen <troy.raen@pitt.edu>
2021-10-29 03:07:57 -0400

    add PittGoogleBaseParser

--- stream/parsers/pittgoogle_parsers.py
+++ stream/parsers/pittgoogle_parsers.py
@@ -10,8 +10,33 @@ from stream.parsers.base_parser import BaseParser
 logger = logging.getLogger(__name__)
 
 
-class PittGoogleAlertParser(BaseParser):
-    # TODO: write this
+class PittGoogleBaseParser(BaseParser):
+    """Base Parser class for Pitt-Google's streams."""
+
+    def __repr__(self):
+        return 'Pitt-Google Base Parser'
 
     def parse(self):
-        pass
+        try:
+            self.parse_metadata()
+            self.parse_alert()
+
+        except Exception as e:
+            logger.warn(f'Unable to parse alert {self.alert} with parser {self}: {e}')
+            return False
+
+        return True
+
+    def parse_metadata(self):
+        metadata_dict = self.alert.metadata
+        self.alert.identifier = str(metadata_dict["message_id"])
+        self.alert.timestamp = metadata_dict["kafka.timestamp"]
+        # self.alert.broker_ingest_timestamp = metadata_dict[""]  # TODO
+        self.alert.broker_publish_timestamp = metadata_dict["publish_time"]
+
+    def parse_alert(self):
+        alert_dict = self.alert.parsed_message
+        # self.alert.identifier = f"{alert_dict['candid']}_PittGoogle"
+        # self.alert.coordinates = (alert_dict["ra"], alert_dict["dec"])  # TODO
+        return
+
