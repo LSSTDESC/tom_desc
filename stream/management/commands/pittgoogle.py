094b071b502a4a186fc941aa14f39d928b400173
troyraen <troy.raen@pitt.edu>
2021-10-29 03:13:42 -0400

    add Alert and parser to listener

--- stream/management/commands/pittgoogle.py
+++ stream/management/commands/pittgoogle.py
@@ -5,11 +5,10 @@ from importlib import import_module
 import logging
 
 from django.conf import settings
-from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
-# from django.db import transaction
+from django.db import transaction
 
-# from stream.models import Alert, Topic
+from stream.models import Alert, Topic
 
 from tom_pittgoogle.consumer_stream_python import ConsumerStreamPython as Consumer
 
@@ -20,26 +19,25 @@ PITTGOOGLE_CONSUMER_CONFIGURATION = settings.PITTGOOGLE_CONSUMER_CONFIGURATION
 PITTGOOGLE_PARSERS = settings.PITTGOOGLE_PARSERS
 
 
-def get_parser_classes(topic):
-    """."""
-    parser_classes = []
-
+def get_parser_class(topic):
+    """Import and return the parser for the given `topic`."""
     try:
-        parsers = PITTGOOGLE_PARSERS[topic]
+        parser = PITTGOOGLE_PARSERS[topic]
     except KeyError:
-        logger.log(msg=f'PITTGOOGLE_PARSERS not found for topic: {topic}.', level=logging.WARNING)
-        return parser_classes
+        logger.log(
+            msg=f'PITTGOOGLE_PARSERS not found, topic: {topic}.', level=logging.WARNING
+        )
+        return None
 
-    for parser in parsers:
-        mod_name, class_name = parser.rsplit('.', 1)
-        try:
-            mod = import_module(mod_name)
-            clazz = getattr(mod, class_name)
-        except (ImportError, AttributeError):
-            raise ImportError(f'Unable to import parser {parser}')
-        parser_classes.append(clazz)
+    mod_name, class_name = parser.rsplit('.', 1)
+    try:
+        mod = import_module(mod_name)
+        clazz = getattr(mod, class_name)
+    except (ImportError, AttributeError):
+        raise ImportError(f'Unable to import parser {parser}')
+    parser_class = clazz
 
-    return parser_classes
+    return parser_class
 
 
 class Command(BaseCommand):
@@ -53,25 +51,47 @@ class Command(BaseCommand):
         -   Execute a "streaming pull" in a background thread.
             Asyncronously, pull alerts and process them through the callback,
             `parse_and_save`.
-
         -   Stop when a stoping condition is reached.
-
         -   Block until the processing is complete and the connection has been closed.
         """
+        consumer = Consumer(PITTGOOGLE_CONSUMER_CONFIGURATION['subscription_name'])
+
         kwargs = {
-                # "desc_group": Group.objects.get(name='DESC'),
-                # "broker": get_service_class('Pitt-Google StreamPython')(),
-                **PITTGOOGLE_CONSUMER_CONFIGURATION,  # stopping conditions
+            # kwargs to be passed to parse_and_save
+            # note that in Pub/Sub, the topic and subscription can have different names
+            "topic_name": consumer.topic_path.rsplit("/", 1),
+            "send_alert_bytes": True,
+            "send_metadata": True,
+            # kwargs for stopping conditions
+            **PITTGOOGLE_CONSUMER_CONFIGURATION,
         }
 
-        consumer = Consumer(PITTGOOGLE_CONSUMER_CONFIGURATION['subscription_name'])
+        # pull and process alerts in a background thread. block until complete.
         _ = consumer.stream_alerts(
             user_callback=self.parse_and_save,
             **kwargs
         )
 
     @staticmethod
-    def parse_and_save(alert_dict, **kwargs):
+    def parse_and_save(alert_dict, alert_bytes, metadata_dict, **kwargs):
         """Parse the alert and save to the database."""
-        # TODO
-        return
+        topic, _ = Topic.objects.get_or_create(name=kwargs["topic_name"])
+        alert = Alert.objects.create(
+            topic=topic,
+            raw_message=alert_bytes,
+            parsed_message=alert_dict,
+            metadata=metadata_dict,
+        )
+
+        parser_class = get_parser_class(topic.name)
+        with transaction.atomic():
+            # Get the parser class, instantiate it, parse the alert, and save it
+            parser = parser_class(alert)
+            alert.parsed = parser.parse()
+            if alert.parsed is True:
+                alert.save()
+
+        # TODO: catch errors
+        success = True
+
+        return success
