c405c27a82f7ca5d7e45947d51904f43da355a08
Alex Kim <agkim@lbl.gov>
2021-11-09 14:06:25 -0800

    new alerts class to be shown in admin

--- stream/admin.py
+++ stream/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Target, Topic, Event, EventAttributes, Alert
+from .models import Target, Topic, ElasticcBrokerAlert, Event, EventAttributes, Alert
 
-admin.site.register([Target, Topic, Event, EventAttributes, Alert])
+admin.site.register([Target, Topic, ElasticcBrokerAlert, Event, EventAttributes, Alert])
