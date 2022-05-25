19dde2b607a8991c6ad691da6193c7d90e7ff64b
Rob Knop (Nersc) <raknop@lbl.gov>
2022-05-25 15:29:01 -0700

    Update diaObject schema.  Have a command that can import BrokerMessages from a kafka server (tested on my own little one).

--- elasticc/models.py
+++ elasticc/models.py
@@ -1,6 +1,8 @@
 import sys
 import math
+import datetime
 import logging
+import itertools
 from django.db import models
 from django.utils.functional import cached_property
 import django.contrib.postgres.indexes as indexes
@@ -11,7 +13,7 @@ _formatter = logging.Formatter( f'[%(asctime)s - %(levelname)s] - %(message)s' )
 _logout.setFormatter( _formatter )
 _logger.propagate = False
 _logger.addHandler( _logout )
-_logger.setLevel( logging.DEBUG )
+_logger.setLevel( logging.INFO )
 
 # Create your models here.
 
@@ -111,18 +113,22 @@ class DiaObject(Createable):
     z_final_err = models.FloatField( null=True )
     hostgal_ellipticity = models.FloatField( null=True )
     hostgal_sqradius = models.FloatField( null=True )
-    hostgal_z = models.FloatField( null=True )
-    hostgal_z_err = models.FloatField( null=True )
-    hostgal_zphot_q10 = models.FloatField( null=True )
-    hostgal_zphot_q20 = models.FloatField( null=True )
-    hostgal_zphot_q30 = models.FloatField( null=True )
-    hostgal_zphot_q40 = models.FloatField( null=True )
-    hostgal_zphot_q50 = models.FloatField( null=True )
-    hostgal_zphot_q60 = models.FloatField( null=True )
-    hostgal_zphot_q70 = models.FloatField( null=True )
-    hostgal_zphot_q80 = models.FloatField( null=True )
-    hostgal_zphot_q90 = models.FloatField( null=True )
-    hostgal_zphot_q99 = models.FloatField( null=True )
+    hostgal_zspec = models.FloatField( null=True )
+    hostgal_zpec_err = models.FloatField( null=True )
+    hostgal_zphot = models.FloatField( null=True )
+    hostgal_zphot_err = models.FloatField( null=True )
+    hostgal_zphot_q000 = models.FloatField( null=True)
+    hostgal_zphot_q010 = models.FloatField( null=True )
+    hostgal_zphot_q020 = models.FloatField( null=True )
+    hostgal_zphot_q030 = models.FloatField( null=True )
+    hostgal_zphot_q040 = models.FloatField( null=True )
+    hostgal_zphot_q050 = models.FloatField( null=True )
+    hostgal_zphot_q060 = models.FloatField( null=True )
+    hostgal_zphot_q070 = models.FloatField( null=True )
+    hostgal_zphot_q080 = models.FloatField( null=True )
+    hostgal_zphot_q090 = models.FloatField( null=True )
+    hostgal_zphot_q100 = models.FloatField( null=True )
+    hostgal_zphot_p50 = models.FloatField( null=True )
     hostgal_mag_u = models.FloatField( null=True )
     hostgal_mag_g = models.FloatField( null=True )
     hostgal_mag_r = models.FloatField( null=True )
@@ -140,18 +146,21 @@ class DiaObject(Createable):
     hostgal_magerr_Y = models.FloatField( null=True )
     hostgal2_ellipticity = models.FloatField( null=True )
     hostgal2_sqradius = models.FloatField( null=True )
-    hostgal2_z = models.FloatField( null=True )
-    hostgal2_z_err = models.FloatField( null=True )
-    hostgal2_zphot_q10 = models.FloatField( null=True )
-    hostgal2_zphot_q20 = models.FloatField( null=True )
-    hostgal2_zphot_q30 = models.FloatField( null=True )
-    hostgal2_zphot_q40 = models.FloatField( null=True )
-    hostgal2_zphot_q50 = models.FloatField( null=True )
-    hostgal2_zphot_q60 = models.FloatField( null=True )
-    hostgal2_zphot_q70 = models.FloatField( null=True )
-    hostgal2_zphot_q80 = models.FloatField( null=True )
-    hostgal2_zphot_q90 = models.FloatField( null=True )
-    hostgal2_zphot_q99 = models.FloatField( null=True )
+    hostgal2_zspec = models.FloatField( null=True )
+    hostgal2_zspec_err = models.FloatField( null=True )
+    hostgal2_zphot = models.FloatField( null=True )
+    hostgal2_zphot_err = models.FloatField( null=True )
+    hostgal2_zphot_q000 = models.FloatField( null=True )
+    hostgal2_zphot_q010 = models.FloatField( null=True )
+    hostgal2_zphot_q020 = models.FloatField( null=True )
+    hostgal2_zphot_q030 = models.FloatField( null=True )
+    hostgal2_zphot_q040 = models.FloatField( null=True )
+    hostgal2_zphot_q050 = models.FloatField( null=True )
+    hostgal2_zphot_q060 = models.FloatField( null=True )
+    hostgal2_zphot_q070 = models.FloatField( null=True )
+    hostgal2_zphot_q080 = models.FloatField( null=True )
+    hostgal2_zphot_q090 = models.FloatField( null=True )
+    hostgal2_zphot_q100 = models.FloatField( null=True )
     hostgal2_mag_u = models.FloatField( null=True )
     hostgal2_mag_g = models.FloatField( null=True )
     hostgal2_mag_r = models.FloatField( null=True )
@@ -177,14 +186,16 @@ class DiaObject(Createable):
     _pk = 'diaObjectId'
     _create_kws = [ 'diaObjectId', 'ra', 'decl', 'mwebv', 'mwebv_err', 'z_final', 'z_final_err' ]
     for _gal in [ "", "2" ]:
-        _create_kws.append( f'hostgal{_gal}_z' )
-        _create_kws.append( f'hostgal{_gal}_z_err' )
+        _create_kws.append( f'hostgal{_gal}_zspec' )
+        _create_kws.append( f'hostgal{_gal}_zspec_err' )
+        _create_kws.append( f'hostgal{_gal}_zspec' )
+        _create_kws.append( f'hostgal{_gal}_zspec_err' )
         _create_kws.append( f'hostgal{_gal}_ra' )
         _create_kws.append( f'hostgal{_gal}_dec' )
         _create_kws.append( f'hostgal{_gal}_snsep' )
         _create_kws.append( f'hostgal{_gal}_ellipticity' )
         _create_kws.append( f'hostgal{_gal}_sqradius' )
-        for _phot in [ 'q10', 'q20', 'q30', 'q40', 'q50', 'q60', 'q70', 'q80', 'q90', 'q99' ]:
+        for _phot in [ 'q000', 'q010', 'q020', 'q030', 'q040', 'q050', 'q060', 'q070', 'q080', 'q090', 'q100' ]:
             _create_kws.append( f'hostgal{_gal}_zphot_{_phot}' )
         for _band in [ 'u', 'g', 'r', 'i', 'z', 'Y' ]:
             for _err in [ '', 'err' ]:
@@ -307,6 +318,39 @@ class DiaTruth(models.Model):
         except DiaTruth.DoesNotExist:
             return DiaTruth.create( data )
 
+class DiaObjectTrut(models.Model):
+    diaObjectId = models.BigIntegerField( )
+    sim_searcheff_mask = models.IntegerField( )
+    gentype = models.IntegerField( )
+    non1a_index = models.IntegerField( )
+    zcmb = models.FloatField( )
+    zhelio = models.FloatField( )
+    zcmb_shear = models.FloatField( )
+    ra = models.FloatField( )
+    dec = models.FloatField( )
+    mwebv = models.FloatField( )
+    galid = models.BigIntegerField( )
+    galzphot = models.FloatField( )
+    galzphoterr = models.FloatField( )
+    rv = models.FloatField( )
+    av = models.FloatField( )
+    mu = models.FloatField( )
+    lensedmu = models.FloatField( )
+    peakmjd = models.FloatField( ) 
+    mjd_trigger = models.FloatField( )
+    peakmag_u = models.FloatField( )
+    peakmag_g = models.FloatField( )
+    peakmag_r = models.FloatField( )
+    peakmag_i = models.FloatField( )
+    peakmag_z = models.FloatField( )
+    peakmag_Y = models.FloatField( )
+    snrmax = models.FloatField( )
+    snrmax2 = models.FloatField( )
+    snrmax3 = models.FloatField( )
+    nobs = models.IntegerField( )
+    nobs_saturate = models.IntegerField( )
+
+
 # ======================================================================
 # The Broker* don't correspond as directly to the avro alerts
 #
@@ -330,12 +374,9 @@ class DiaTruth(models.Model):
 #   dbMessageIndex (just the primary key, auto updated)
 #   descIngestTimestamp
 #   modified
-# I'm not fully sure what to do with:
-#   streamMessageId
-#   topicName
-# --> these seem to assume that the message came from an kafka stream
-#     Probably I should make them nullable in case they didn't come from there.
-#     or, another table to track kafka stream topic etc.  ROB THINK.
+#   streamMessageId (pulled from the .offset() method of the message we get from kafka consumer)...
+#                      ...may not be terribly meaningful because of partitions!  Figure this out)
+#   topicName (pulled from the .topic() method of the message we get from the kafka consumer)
 #
 # In addition, in the avro alert, there is further stuff.  When adding a
 # message to the database, We will dig into the classifications array
@@ -372,10 +413,140 @@ class BrokerMessage(models.Model):
         indexes = [
             # models.Index(fields=['dbMessageIndex', 'topicName']),
             models.Index( fields=[ 'dbMessageIndex' ] ),
+            models.Index( fields=[ 'topicName', 'streamMessageId' ] ),
             models.Index( fields=[ 'alertId' ] ),
             models.Index( fields=[ 'diaSourceId' ] ),
         ]
 
+
+    @staticmethod
+    def load_batch( messages ):
+        """Load an array of messages into BrokerMessage and associated tables.
+
+        messages is an array of dicts with keys topic, msgoffset, and msg.
+        topic is string, msgoffset is an integer, and msg is a dict that should
+        match the elasticc.v0_9.brokerClassification.avsc schema
+
+        Returns the number of things that were actually added.
+        """
+
+        # It's a hard problem to decide if a message already
+        # exists.  The things that are in the BrokerMessage object
+        # could (by chance) be duplicated for different brokers.
+        # I need to think about this, but for now I'm going to assume
+        # that all messages coming in are new messages.  I *think*
+        # at worst that this will lead to useless message objects
+        # in the database that we can ignore.  I think.
+
+        _logger.debug( f'In BrokerMessage.load_batch, received {len(messages)} messages.' );
+
+        messageobjects = {}
+        kwargses = []
+        for msg in messages:
+            if len( msg['msg']['classifications'] ) == 0:
+                _logger.debug( "Message with no classifications" )
+                continue
+            keymess = ( f"{msg['msgoffset']}_{msg['topic']}_{msg['msg']['alertId']}" )
+            _logger.debug( f'kemess = {keymess}' )
+            if keymess not in messageobjects.keys():
+                kwargs = { 'streamMessageId': msg['msgoffset'],
+                           'topicName': msg['topic'],
+                           'alertId': msg['msg']['alertId'],
+                           'diaSourceId': msg['msg']['diaSourceId'],
+                           'descIngestTimestamp': datetime.datetime.now(),
+                           'elasticcPublishTimestamp': msg['msg']['elasticcPublishTimestamp'],
+                           'brokerIngestTimestamp': msg['msg']['brokerIngestTimestamp'],
+                           # 'elasticcPublishTimestamp': datetime.datetime.fromtimestamp(
+                           #     msg['msg']['elasticcPublisTimestamp'] / 1000000 ),
+                           # 'brokerIngestTimestamp': datetime.datetime.fromtimestamp(
+                           #     msg['msg']['brokerIngestTimestamp'] / 1000000 )
+                }
+                kwargses.append( kwargs )
+            else:
+                _logger.error( f'Key {keymess} showed up more than once in a message batch!' )
+        _logger.debug( f'Bulk creating {len(kwargses)} messages.' )
+        if len(kwargses) > 0:
+            # This is byzantine, but I'm copying django documentation here
+            objs = ( BrokerMessage( **k ) for k in kwargses )
+            batch = list( itertools.islice( objs, len(kwargses) ) )
+            if batch is None:
+                raise RunTimeError( "Something bad has happened." )
+            addedmsgs = BrokerMessage.objects.bulk_create( batch, len(kwargses) )
+            for addedmsg in addedmsgs:
+                keymess = f"{addedmsg.streamMessageId}_{addedmsg.topicName}_{addedmsg.alertId}"
+                messageobjects[ keymess ] = addedmsg
+
+        # Figure out which classifiers already exist.
+        # I need to figure out if there's a way to tell Django
+        # to do a WHERE...IN on tuples.  For now, hopefully this
+        # Q object thing won't be a disaster
+
+        classifiers = {}
+        
+        cferconds = models.Q()
+        for msg in messages:
+            for cfication in msg['msg']['classifications']:
+                newcond = ( models.Q( brokerName = msg['msg']['brokerName'] ) &
+                            models.Q( brokerVersion = msg['msg']['brokerVersion'] ) &
+                            models.Q( classifierName = cfication['classifierName'] ) &
+                            models.Q( classifierParams = cfication['classifierParams'] ) )
+                cferconds |= newcond
+            curcfers = BrokerClassifier.objects.filter( cferconds )
+            for cur in curcfers:
+                keycfer = ( f"{cur.brokerName}_{cur.brokerVersion}_"
+                            f"{cur.classifierName}_{cur.classifierParams}" )
+                classifiers[ keycfer ] = cur
+        _logger.debug( f'Found {len(classifiers)} existing classifiers.' )
+                
+        # Create new classifiers as necessary
+
+        addedkeys = set()
+        kwargses = []
+        for msg in messages:
+            for cfication in msg['msg']['classifications']:
+                keycfer = ( f"{msg['msg']['brokerName']}_{msg['msg']['brokerVersion']}_"
+                            f"{cfication['classifierName']}_{cfication['classifierParams']}" )
+                if ( keycfer not in classifiers.keys() ) and ( keycfer not in addedkeys ):
+                    kwargses.append( { 'brokerName': msg['msg']['brokerName'],
+                                       'brokerVersion': msg['msg']['brokerVersion'],
+                                       'classifierName': cfication['classifierName'],
+                                       'classifierParams': cfication['classifierParams'] } )
+                    addedkeys.add( keycfer )
+        _logger.debug( f'Adding {len(kwargses)} new classifiers.' )
+        if len(kwargses) > 0:
+            objs = ( BrokerClassifier( **k ) for k in kwargses )
+            batch = list( itertools.islice( objs, len(kwargses) ) )
+            newcfers = BrokerClassifier.objects.bulk_create( batch, len(kwargses) )
+            for curcfer in newcfers:
+                keycfer = ( f"{curcfer.brokerName}_{curcfer.brokerVersion}_"
+                            f"{curcfer.classifierName}_{curcfer.classifierParams}" )
+                classifiers[ keycfer ] 
+
+        # Add the new classifications
+        #
+        # ROB TODO : think about duplication!  Right now I'm just
+        # assuming I won't get any.
+
+        kwargses = []
+        for msg in messages:
+            if len( msg['msg']['classifications'] ) == 0:
+                continue
+            keymess = ( f"{msg['msgoffset']}_{msg['topic']}_{msg['msg']['alertId']}" )
+            for cficiation in msg['msg']['classifications']:
+                keycfer = ( f"{msg['msg']['brokerName']}_{msg['msg']['brokerVersion']}_"
+                            f"{cfication['classifierName']}_{cfication['classifierParams']}" )
+                kwargs = { 'dbMessage': messageobjects[keymess],
+                           'dbClassifier': classifiers[keycfer],
+                           'classId': cfication['classId'],
+                           'probability': cfication['probability'] }
+                kwargses.append( kwargs )
+        _logger.debug( f'Adding {len(kwargses)} new classifications.' )
+        objs = ( BrokerClassification( **k ) for k in kwargses )
+        batch = list( itertools.islice( objs, len(kwargses) ) )
+        newcfications = BrokerClassification.objects.bulk_create( batch, len(kwargses) )
+
+        return len(newcfications)
+        
 class BrokerClassifier(models.Model):
     """Model for a classifier producing an ELAsTiCC broker classification."""
 
@@ -390,6 +561,8 @@ class BrokerClassifier(models.Model):
 
     class Meta:
         indexes = [
+            models.Index(fields=["brokerName"]),
+            models.Index(fields=["brokerName", "brokerVersion"]),
             models.Index(fields=["brokerName", "classifierName"]),
         ]
 
