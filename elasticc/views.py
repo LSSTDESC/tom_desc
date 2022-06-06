1a76b894a082f1c6b337532434b8af69fd7b9c78
Rob Knop (Nersc) <raknop@lbl.gov>
2022-06-06 08:00:02 -0700

    Viewing broker messages

--- elasticc/views.py
+++ elasticc/views.py
@@ -6,6 +6,7 @@ import json
 import time
 import datetime
 import logging
+import django.db
 import django.urls
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
@@ -252,55 +253,96 @@ class BrokerMessageView(PermissionRequiredMixin, django.views.View):
         else:
             return bool(self.request.user.is_authenticated)
 
-    def get_one_brokermessage( self, request, msgid ):
-        msgobj = BrokerMessage.objects.get(pk=msgid)
-        if msgobj is None:
-            return JsonResponse( {} )
+    def get_queryset( self, request, info, offset=0, num=100 ):
+        n = None
+        if isinstance( info, int ):
+            msgs = BrokerMessage.objects.filter(pk=info)
+
         else:
-            return JsonResponse( msgobj.to_dict() )
+            argre = re.compile( '^([a-z0-9]+)=(.*)$' )
+            args = {}
+            argstr = str(info).split("/")
+            for argtxt in argstr:
+                match = argre.search( argtxt )
+                if match is not None:
+                    args[match.group(1).lower()] = match.group(2)
+                else:
+                    args[argtxt] = None
 
-    def get_brokermessage_for_alertid( self, alertid ):
-        msgobjs = BrokerMessage.objects.filter( alertId__in==str(alertid).split(",") )
-        return JsonResponse( [ msg.to_dict() for msg in msgobjs ], safe=False )
+            if ( len(args) == 1 ) and ( "alertid" in args.keys() ):
+                alertid = args['alertid']
+                msgs = BrokerMessage.objects.filter( alertId__in==str(alertid).split(",") )
 
-    def get_brokermessage_for_sourceid( self, sourceid ):
-        msgobjs = BrokerMessage.objects.filter( diaSourceId__in=str(sourceid).split(",") )
-        return JsonResponse( [ msg.to_dict() for msg in msgobjs ], safe=False )
+            elif ( len(args) == 1 ) and ( ( "objectid" in args.keys() ) or ( "diaobjectid" in args.keys() ) ):
+                objid = list(args.values())[0]
+                # django doesn't seem to have a native way to do joins on things that aren't foreign keys
+                # So, I need to do this raw thing.
+                # This also means I have to do a separate query to get the count, hence the "n=None"
+                # business at the top.
+                params={ 'objids': tuple( str(objid).split(",") ) }
+                with django.db.connection.cursor() as cursor:
+                    cursor.execute( 'SELECT COUNT(b."dbMessageIndex") FROM elasticc_brokermessage b'
+                                    ' INNER JOIN elasticc_diasource s ON b."diaSourceId"=s."diaSourceId"'
+                                    ' WHERE s."diaObject_id" IN %(objids)s', params=params )
+                    row = cursor.fetchone()
+                    n = row[0]
+                msgs = BrokerMessage.objects.raw(
+                    'SELECT * FROM elasticc_brokermessage b INNER JOIN elasticc_diasource s'
+                    ' ON b."diaSourceId"=s."diaSourceId" WHERE s."diaObject_id" IN %(objids)s', params=params )
 
-    def get_brokermessage_for_objectid( self, objid ):
-        # django doesn't seem to have a native way to do joins on things that aren't foreign keys
-        msgobjs = BrokerMessage.objects.raw(
-            'SELECT * FROM elasticc_brokermessage b INNER JOIN elasticc_diasource s'
-            ' ON b."diaSourceId"=s."diaSourceId" WHERE s."diaObject_id" IN %(objids)s',
-            params={ 'objids': tuple( str(objid).split(",") ) } )
-        return JsonResponse( [ msg.to_dict() for msg in msgobjs ], safe=False )
-    
+            elif ( len(args) == 1 ) and ( ( "sourceid" in args.keys() ) or ( "diasourceid" in args.keys() ) ):
+                sourceid = list(args.values())[0]
+                msgs = BrokerMessage.objects.filter( diaSourceId__in=str(sourceid).split(",") )
+
+            else:
+                raise ValueError( f"Can't parse argument string \"{info}\"." )
+
+        if n is None:
+            n = msgs.count()
+            
+        if ( offset is None ) and ( num is None ):
+            return msgs, n
+        elif num is None:
+            return msgs[offset:], n
+        elif offset is None:
+            return msgs[0:num], n
+        else:
+            return msgs[offset:offset+num], n
+
+    def offset_num( self, request ):
+        vals = { 'offset': 0,
+                 'num': 100 }
+        for args in [ request.GET, request.POST ]:
+            for val in vals.keys():
+                if val in args.items():
+                    if args[val] == "None":
+                        vals[val] = None
+                    else:
+                        vals[val] = int(args[val])
+        return vals['offset'], vals['num']
+        
     def get( self, request, info=None ):
-        if info is None:
-            return JsonResponse( { "error": f"Must qualify request" } )
+        # EVentually I want to make get return html
+        return self.post( request, info )
+        
+    def post( self, request, info=None ):
+        offset, num = self.offset_num( request )
+
+        try:
+            msgs, nmsgs = self.get_queryset( request, info, offset, num )
+        except ValueError as ex:
+            return JsonResponse( { "error": str(ex) } )
         
         if isinstance( info, int ):
-            return self.get_one_brokermessage( request, info )
-
-        argre = re.compile( '^([a-z0-9]+)=(.*)$' )
-        args = {}
-        argstr = str(info).split("/")
-        for argtxt in argstr:
-            match = argre.search( argtxt )
-            if match is not None:
-                args[match.group(1).lower()] = match.group(2)
-            else:
-                args[argtxt] = None
-
-        if ( len(args) == 1 ) and ( "alertid" in args.keys() ):
-            return self.get_brokermessage_for_alertid( args['alertid'] )
-        elif ( len(args) == 1 ) and ( ( "objectid" in args.keys() ) or ( "diaobjectid" in args.keys() ) ):
-            return self.get_brokermessage_for_objectid( list(args.values())[0] )
-        elif ( len(args) == 1 ) and ( ( "sourceid" in args.keys() ) or ( "diasourceid" in args.keys() ) ):
-            return self.get_brokermessage_for_sourceid( list(args.values())[0] )
+            return JsonResponse( {} if msgs.count()==0 else msgs[0].to_dict() )
         else:
-            return JsonResponse( { "error": f"Can't parse argument string \"{info}\"." } )
-    
+            return JsonResponse( { 'offset': offset,
+                                   'num': num,
+                                   'totalcount': nmsgs,
+                                   'count': msgs.count(),
+                                   'brokermessages': [ msg.to_dict() for msg in msgs ] } )
+        
+        
     def put( self, request, *args, **kwargs ):
         data = json.loads( request.body )
 
